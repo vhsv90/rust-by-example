9d64d7590bb9d6b2e7b930fd4f07bb3bc40f773d
Victor Sanchez <vhsv90@gmail.com>
2024-02-04 09:50:07 -0600

    Rust | Hello World Exercise

---
+++ hello-world/helloWorld.rs
@@ -0,0 +1,12 @@
+// This is a comment, and is ignored by the compiler
+
+// You can compile this file using command
+// rustc helloWorld.rs
+
+// You can execute this file using command
+// ./helloWorld
+
+// This is the Main functions
+fn main() {
+    println!("Hello World by vhs90!");
+}
